2a17921551a027dbc64e67abaec90871dae18ba7
OhHyemi <gpal1014@naver.com>
2021-11-15 03:02:43 +0900

    making button directing

---
+++ UIDirectingPractice/Assets/MyProj/Scripts/ButtonDirecting.cs
@@ -0,0 +1,63 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class ButtonDirecting : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+{
+    public AnimationCurve ease;
+    public Vector2 toSize = Vector2.one;
+    public float duration;
+    private bool isTweening = false;
+    private event Action onClick;
+
+    private Vector2 fromSize;
+
+    private void Awake()
+    {
+        fromSize = transform.localScale;
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        if (isTweening == true)
+        {
+            return;
+        }
+
+        StartCoroutine(CoAnimateScale());
+
+    }
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        if (onClick != null)
+        {
+            onClick();
+        }
+    }
+
+    public void AddListener(Action onClick)
+    {
+        this.onClick += onClick;
+    }
+
+    IEnumerator CoAnimateScale()
+    {
+        isTweening = true;
+        
+        float time = 0;
+        while (time < duration)
+        {
+            transform.localScale =Vector2.Lerp(toSize, toSize * 0.9f, ease.Evaluate(time / duration));
+
+            time += Time.deltaTime;
+            
+            yield return null;
+        }
+        isTweening = false;
+        transform.localScale = fromSize;
+    }
+
+}
